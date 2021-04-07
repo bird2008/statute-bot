2dfbb3d1aaf5f22eccc30f00514d9b82e2ff60c8
bird2008 <80196280+bird2008@users.noreply.github.com>
2021-04-07 08:01:43 +0200

    add invite log

--- commands/statute.commands.js
+++ commands/statute.commands.js
@@ -9,7 +9,7 @@ module.exports = {
         .setTitle("**REGULAMIN**")
         .setColor(0xcc2c2c)
         .setDescription("**1.📝WYSYŁANIE LINKÓW DO INNEGO SERWERA GROZI BANEM PERMAMENTNYM**\n**2.💎SPAMIENIE NIE POTRZEBNIE BEZ POWODU !MUTE {CZAS}**\n**3.💜NIE WYZYWAJ INNYCH GRACZ, GROZI TO BANEM NA 24H**\n**4.🏆UŻYWANIE MODULATORA GŁOSU BAN 12D**\n**5.🎉WYZYWANIE ADMINISTRACJI BAN {OKREŚLONY CZAS}**\n**6.🔊WYSYWAŁNIE PRYWATNYCH RZECZY " + "|KOGOŚ|" + "BAN=PERM**\n**7.✨NICK NIE DO OZNACZENIA" + "@" + "BAN 2D**")
-        .addField("Autor", "\_! akompostownik_", true)
+        .addField("Autor", "\_Elish_", true)
         msg.channel.send(embed) 
     }
 }    
